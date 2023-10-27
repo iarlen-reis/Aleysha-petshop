f2547a17c88e38c6804cfdf596db305b16c8061a
Iarlen <iarlensr@hotmail.com>
2023-10-27 04:12:02 -0300

    feat: create LoginButtonContainer component.

---
+++ src/components/Login/LoginButtonContainer/index.tsx
@@ -0,0 +1,36 @@
+'use client'
+import React from 'react'
+import { signIn } from 'next-auth/react'
+import { BsGithub } from 'react-icons/bs'
+import { AiFillGoogleCircle } from 'react-icons/ai'
+import ButtonLogin from '@/components/Login/ButtonLogin'
+
+const LoginButtonContainer = () => {
+  const handleLoginWithGithub = () => {
+    signIn('github')
+  }
+
+  const handleLoginWithGoogle = () => {
+    signIn('google')
+  }
+
+  return (
+    <div className="w-full flex flex-col gap-4 items-center">
+      <ButtonLogin
+        text="Faça login com o GitHub"
+        icon={BsGithub}
+        aditionalStyles="bg-background-button"
+        onClick={handleLoginWithGithub}
+      />
+      <span className="font-ruluko text-lg italic lg:text-xl">ou</span>
+      <ButtonLogin
+        text="Faça login com o Google"
+        icon={AiFillGoogleCircle}
+        aditionalStyles="bg-[#EF4444]"
+        onClick={handleLoginWithGoogle}
+      />
+    </div>
+  )
+}
+
+export default LoginButtonContainer
