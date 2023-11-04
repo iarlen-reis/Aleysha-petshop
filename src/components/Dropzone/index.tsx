61d07cd47cb26a12939209bcd4ff0bbbb070d27d
Iarlen <iarlensr@hotmail.com>
2023-11-04 19:17:52 -0300

    feat: created Dropzone component to upload image.

---
+++ src/components/Dropzone/index.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import Image from 'next/image'
+import React, { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+
+interface DropzoneProps {
+  setFile: React.Dispatch<React.SetStateAction<string>>
+  file: string | null
+  loading: boolean
+}
+const Dropzone = ({ setFile, file, loading }: DropzoneProps) => {
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      const file = acceptedFiles[0]
+      const reader = new FileReader()
+
+      reader.readAsDataURL(file)
+
+      reader.onload = () => {
+        setFile(reader.result as unknown as string)
+      }
+    },
+    [setFile],
+  )
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+  const handleRemoveImage = () => {
+    setFile('')
+  }
+
+  return (
+    <div>
+      {file && (
+        <div className="relative">
+          <Image
+            src={file}
+            alt="Imagem do produto"
+            width={300}
+            height={300}
+            className="max-w-[200px] max-h-[200px] w-full mx-auto rounded-[50%] object-cover"
+          />
+          {!loading && (
+            <button
+              className="absolute top-0 right-0 z-10 text-xl transition-all duration-200 hover:text-red-500"
+              onClick={handleRemoveImage}
+            >
+              X
+            </button>
+          )}
+        </div>
+      )}
+      {!file && (
+        <div
+          {...getRootProps()}
+          className="w-full text-center bg-background-input rounded-md border border-black/20 p-3"
+        >
+          <input {...getInputProps()} accept="image/*" />
+          {isDragActive ? (
+            <p>a</p>
+          ) : (
+            <p className="text-lg font-ruluko text-center">
+              Arraste e solte uma imagem ou <br /> clique para fazer o upload...
+            </p>
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Dropzone
