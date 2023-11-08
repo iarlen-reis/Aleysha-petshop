3d48bfc9a0981dc8c2f0f94f7720e9815e0dad46
Iarlen <iarlensr@hotmail.com>
2023-11-08 20:54:39 -0300

    chore: installing auth/prisma-adapter, configuring auth/prisma-adapter, extending Session interface and adding assets to folder cart.

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,8 +1,11 @@
 import { NextAuthOptions } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { prisma } from './prisma'
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
@@ -13,6 +16,12 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id
+      return Promise.resolve(session)
+    },
+  },
   secret: process.env.NEXTAUTH_SECRET as string,
   pages: {
     signIn: '/login',
