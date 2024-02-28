2f3ec85653c2ce3264532ef0a0459d100480e577
Iarlen <iarlensr@hotmail.com>
2024-02-27 21:36:30 -0300

    refactor: refactoring the chart formatting routes api and deleting the useCharts hook.

--- src/app/api/charts/route.ts
+++ src/app/api/charts/route.ts
@@ -1,9 +1,18 @@
-import prisma from '@/utils/prisma'
-import { NextResponse } from 'next/server'
+import prisma from "@/utils/prisma";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
   try {
-    const currentYear = new Date().getFullYear()
+    const currentYear = new Date().getFullYear();
+    const page = Number(request.nextUrl.searchParams.get("page")) || 1;
+    
+    
+    const CHARTS_PER_PAGE = 5;
+    const totalProducts = await prisma.product.count();
+
+    const maxPage = Math.ceil(totalProducts / CHARTS_PER_PAGE);
+    const existNextPage = page < maxPage;
+    const existPreviousPage = page > 1;
 
     const yearlyOrderProducts = await prisma.oderProduct.findMany({
       where: {
@@ -17,10 +26,15 @@ export async function GET() {
         createdAt: true,
         quantity: true,
       },
-    })
+      orderBy: {
+        createdAt: "desc",
+      },
+      take: CHARTS_PER_PAGE,
+      skip: (page - 1) * CHARTS_PER_PAGE,
+    });
 
-    const monthlyOrderProductsMap = new Map()
-    const productDetailsMap = new Map()
+    const monthlyOrderProductsMap = new Map();
+    const productDetailsMap = new Map();
 
     await Promise.all(
       yearlyOrderProducts.map(async (product) => {
@@ -31,42 +45,47 @@ export async function GET() {
             },
             select: {
               name: true,
+              type: true,
             },
-          })
+          });
           productDetailsMap.set(
             product.productId,
-            details?.name || 'Produto Desconhecido',
-          )
+            details?.name || "Produto Desconhecido"
+          );
         }
-      }),
-    )
+      })
+    );
 
     yearlyOrderProducts.forEach((product) => {
-      const productId = product.productId
-      const month = new Date(product.createdAt).getMonth() + 1
-      const quantity = product.quantity
+      const productId = product.productId;
+      const month = new Date(product.createdAt).getMonth() + 1;
+      const quantity = product.quantity;
 
       if (!monthlyOrderProductsMap.has(productId)) {
         monthlyOrderProductsMap.set(
           productId,
-          Array.from({ length: 12 }).fill(null),
-        )
+          Array.from({ length: 12 }).fill(0)
+        );
       }
 
-      monthlyOrderProductsMap.get(productId)[month - 1] += quantity
-    })
+      monthlyOrderProductsMap.get(productId)[month - 1] += quantity;
+    });
 
-    const monthlyOrderProducts = Array.from(
-      monthlyOrderProductsMap.entries(),
+    const charts = Array.from(
+      monthlyOrderProductsMap.entries()
     ).map(([productId, quantities]) => ({
       productId,
-      name: productDetailsMap.get(productId),
       data: quantities,
-    }))
+      name: productDetailsMap.get(productId),
+      quantity: quantities.reduce((a: number, b: number) => a + b, 0),
+    }));
 
-    return NextResponse.json(monthlyOrderProducts, { status: 200 })
+    return NextResponse.json(
+      { maxPage, existNextPage, existPreviousPage, charts },
+      { status: 200 }
+    );
   } catch (error) {
-    console.log(error)
-    return NextResponse.json({ message: error }, { status: 500 })
+    console.log(error);
+    return NextResponse.json({ message: error }, { status: 500 });
   }
 }
