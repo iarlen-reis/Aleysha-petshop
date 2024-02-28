16a13e46be423c57d2d4c372fc68165b47d86a7d
Iarlen <iarlensr@hotmail.com>
2024-02-27 21:53:39 -0300

    feat: adding pagination on orders pages.

--- src/app/pedidos/page.tsx
+++ src/app/pedidos/page.tsx
@@ -1,15 +1,35 @@
-import React from 'react'
-import Image from 'next/image'
-import prisma from '@/utils/prisma'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/utils/authOptions'
-import OrderCard from '@/components/app/Order/OrderCard'
-import PageNavigation from '@/components/PageNavigation'
-import NoOrderImage from '/public/images/order/no-order.png'
+import React from "react";
+import Image from "next/image";
+import prisma from "@/utils/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/utils/authOptions";
+import OrderCard from "@/components/app/Order/OrderCard";
+import PageNavigation from "@/components/PageNavigation";
+import NoOrderImage from "/public/images/order/no-order.png";
+import { Pagination } from "@/components/Pagination";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 
+interface ParamProps {
+  searchParams: {
+    page: string;
+  };
+}
 
-const OrderPage = async () => {
-  const session = await getServerSession(authOptions)
+const OrderPage = async ({ searchParams }: ParamProps) => {
+  const page = Number(searchParams.page) || 1;
+
+  const session = await getServerSession(authOptions);
+  const ORDERS_PER_PAGE = 5;
+
+  const totalOrders = await prisma.order.count({
+    where: {
+      userId: session?.user.id,
+    },
+  });
+
+  const maxPage = Math.ceil(totalOrders / ORDERS_PER_PAGE);
+  const existNextPage = page < maxPage;
+  const existPreviousPage = page > 1;
 
   const orders = await prisma.order.findMany({
     where: {
@@ -23,9 +43,11 @@ const OrderPage = async () => {
       },
     },
     orderBy: {
-      createdAt: 'desc',
+      createdAt: "desc",
     },
-  })
+    take: ORDERS_PER_PAGE,
+    skip: (page - 1) * ORDERS_PER_PAGE,
+  });
 
   return (
     <div className="min-h-screen flex flex-col gap-6 pb-12">
@@ -36,13 +58,31 @@ const OrderPage = async () => {
           backText="Produtos"
         />
       </div>
-      {orders ? (
+      {orders && (
         <ul className="flex flex-col gap-4 mt-6">
           {orders.map((order) => (
             <OrderCard key={order.id} {...order} />
           ))}
         </ul>
-      ) : (
+      )}
+      <Pagination.Root>
+        {existPreviousPage && (
+          <Pagination.LinkContainer>
+            <Pagination.Link url={`/pedidos?page=${page - 1}`}>
+              <Pagination.Icon icon={ChevronLeftIcon} />
+            </Pagination.Link>
+          </Pagination.LinkContainer>
+        )}
+        {maxPage > 1 && <Pagination.Indicator page={page} />}
+        {existNextPage && (
+          <Pagination.LinkContainer>
+            <Pagination.Link url={`/pedidos?page=${page + 1}`}>
+              <Pagination.Icon icon={ChevronRightIcon} />
+            </Pagination.Link>
+          </Pagination.LinkContainer>
+        )}
+      </Pagination.Root>
+      {!orders && (
         <div className="max-w-[300px] w-full mx-auto flex flex-col items-center gap-4 sm:max-w-[400px]">
           <Image src={NoOrderImage} alt="Imagem de uma mulher no celular" />
           <p className="font-ruluko text-center text-2xl md:text-3xl">
@@ -51,7 +91,7 @@ const OrderPage = async () => {
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default OrderPage
+export default OrderPage;
